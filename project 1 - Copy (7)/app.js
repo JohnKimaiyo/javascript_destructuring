9ec8bc1c533cf3ab456d054ae5f064c73fc2e76e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-01 21:08:02 +0300

    javascript destructuring

---
+++ project 1 - Copy (7)/app.js
@@ -0,0 +1,16 @@
+// array destructuring
+let arr = ["john", "smith"];
+let [firstName, sirName] = arr;
+
+console.log(firstName);
+console.log(sirName);
+
+// object destructuring
+const developer = {
+  front: "react",
+  backend: "node",
+  database: "mysql",
+};
+
+const { frontend, backend, database } = developer;
+console.log(frontend, backend, database);
