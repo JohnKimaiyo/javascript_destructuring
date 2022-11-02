3c521704b43932c7e029d98b04716370eb6b87c7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-02 15:49:42 +0300

    javascript destructing

---
+++ project 1 - Copy (20)/app.js
@@ -0,0 +1,15 @@
+// array desctruing
+let arr = ["john", "smith"];
+let [firstname, sirname] = arr;
+console.log(firstname);
+console.log(sirname);
+
+// object desctruing
+const developer = {
+  frontend: "react",
+  backend: "node",
+  database: "mysql",
+};
+
+const { frontend, backend, database } = developer;
+console.log(frontend, backend, database);
